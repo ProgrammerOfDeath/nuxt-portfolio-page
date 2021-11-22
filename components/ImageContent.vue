20550a339f33b0890bbfdb4ec262250d8506967c
KernelPony <rene.hotho@advocado.com>
2021-11-22 22:16:30 +0100

    components: add new components for image + content sections and navigation

---
+++ components/ImageContent.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="image-content" :class="direction">
+    <div class="columns">
+      <div class="column">
+        <img :src="imageSrc" alt="Image">
+      </div>
+      <div class="column">
+        {{ textContent }}
+      </div>
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    direction: {
+      type: String,
+      default: "ltr"
+    },
+    imageSrc: {
+      type: String,
+      required: true
+    },
+    textContent: {
+      type: String,
+      required: true
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/TheNavigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="container">
+    <b-navbar>
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{path: '/'}">
+          <p>rene.hotho.dev</p>
+        </b-navbar-item>
+      </template>
+      <template #end>
+        <b-navbar-item>{{ $t('navLinks.aboutMe') }}</b-navbar-item>
+        <b-navbar-item>{{ $t('navLinks.cv') }}</b-navbar-item>
+        <b-navbar-item>{{ $t('navLinks.projects') }}</b-navbar-item>
+      </template>
+    </b-navbar>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
